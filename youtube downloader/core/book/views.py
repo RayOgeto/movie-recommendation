89ce13c2174bade0e4c3a4f8e6768ad8239cd111
Ogeto <128529806+RayOgeto@users.noreply.github.com>
2024-05-20 00:06:28 +0300

    Add files via upload

---
+++ youtube downloader/core/book/views.py
@@ -0,0 +1,114 @@
+from django.shortcuts import render, redirect
+from .models import Emenitites, Movie
+from django.http import JsonResponse
+from django.contrib import messages
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth import logout
+import requests
+from django.conf import settings
+from django.http import JsonResponse
+from django.shortcuts import render
+
+def get_movie_recommendations(request):
+    url = "https://movie-and-show-recommendation.p.rapidapi.com/recommend/movie"
+    headers = {
+        'x-rapidapi-key': settings.RAPIDAPI_KEY,
+        'x-rapidapi-host': settings.RAPIDAPI_HOST
+    }
+    
+    movie_id = request.GET.get('movie_id', 'tt1375666')  # default to Inception's IMDb ID
+    # movie_name = request.GET.get('movie_name', 'Inception')
+	
+    # querystring = {"q": movie_name}
+    querystring = {"tconst": movie_id}
+
+    response = requests.get(url, headers=headers, params=querystring)
+    data = response.json()
+    
+    return JsonResponse(data, safe=False)
+
+def home(request):
+    return render(request, 'home.html')
+
+
+@login_required(login_url="/login/")
+def home(request):
+	emenities = Emenitites.objects.all()
+	context = {'emenities': emenities}
+	return render(request, 'home.html', context)
+
+@login_required(login_url="/login/")
+def api_movies(request):
+	movies_objs = Movie.objects.all()
+	
+	price = request.GET.get('price')
+	if price:
+		movies_objs = movies_objs.filter(price__lte=price)
+		
+	emenities = request.GET.get('emenities')
+	if emenities:
+		emenities = [int(e) for e in emenities.split(',') if e.isdigit()]
+		movies_objs = movies_objs.filter(emenities__in=emenities).distinct()
+	
+	payload = [{'movie_name': movie_obj.movie_name,
+				'movie_description': movie_obj.movie_description,
+				'movie_image': movie_obj.movie_image,
+				'price': movie_obj.price} for movie_obj in movies_objs]
+	
+	return JsonResponse(payload, safe=False)
+
+def login_page(request):
+	if request.method == "POST":
+		try:
+			username = request.POST.get('username')
+			password = request.POST.get('password')
+			user_obj = User.objects.filter(username=username)
+			
+			if not user_obj.exists():
+				messages.error(request, "Username not found")
+				return redirect('/login/')
+			
+			user_obj = authenticate(username=username, password=password)
+			
+			if user_obj:
+				login(request, user_obj)
+				return redirect('/')
+			
+			messages.error(request, "Wrong Password")
+			return redirect('/login/')
+			
+		except Exception as e:
+			messages.error(request, "Something went wrong")
+			return redirect('/register/')
+		
+	return render(request, "login.html")
+
+def register_page(request):
+	if request.method == "POST":
+		try:
+			username = request.POST.get('username')
+			password = request.POST.get('password')
+			user_obj = User.objects.filter(username=username)
+			
+			if user_obj.exists():
+				messages.error(request, "Username is taken")
+				return redirect('/register/')
+			
+			user_obj = User.objects.create(username=username)
+			user_obj.set_password(password)
+			user_obj.save()
+			
+			messages.success(request, "Account created")
+			return redirect('/login/')
+		
+		except Exception as e:
+			messages.error(request, "Something went wrong")
+			return redirect('/register/')
+	
+	return render(request, "register.html")
+
+def custom_logout(request):
+	logout(request)
+	return redirect('login')
